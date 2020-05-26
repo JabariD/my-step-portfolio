4665d79d93c4d21d923fb64ad150aba5b781a7ab
Payton <dennispayton15@gmail.com>
2020-05-26 14:59:51 -0500

    Personalize Portfolio, adding color and basic functionality

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!', 'Привет мир!', 'Hello Wêreld!'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
@@ -25,4 +25,18 @@ function addRandomGreeting() {
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
+}
+
+/**
+ * Changes color of the ONLY the main header on the page.
+ */
+function applyColor() {
+    const colors =
+      ['red', 'green', 'blue', 'yellow', 'black', 'purple', 'orange', 'pink', 'gray', 'aqua', 'tomato', 'teal'];
+    
+    // Pick a random color.
+    const color = colors[Math.floor(Math.random() * colors.length)];
+
+    // Add it to the page.
+    document.getElementById("header").style.color = color;
 }
