2ab8f06598a5ebc5722ca160005ecd95ba4b9c60
Payton <dennispayton15@gmail.com>
2020-06-16 19:46:07 +0000

    Implement FindMeetingQuery.java
    Find all possible meeting times based on the current events on the schedule.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,108 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    // If: 1) No attendees. OR 2) There are no events currently on calendar and the meeting duration is less than whole day. Therefore whole day is open for that meeting.
+    if (request.getAttendees().isEmpty() || (events.isEmpty() && request.getDuration() <= TimeRange.WHOLE_DAY.duration())) return Arrays.asList(TimeRange.WHOLE_DAY);
+
+    // Check that duration of meeting is within boundaries of a day
+    else if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) return Arrays.asList();
+
+    // Convert Collection to ArrayList
+    ArrayList<Event> listOfEvents = new ArrayList<Event>(events);
+    
+    //  Check if we have no attendees. If we have just 1 person attending any event from any part of the day, then we cannot return the whole day.
+    ArrayList<String> requestedEventAttendees = new ArrayList<String>(request.getAttendees());
+
+    Boolean noMatchingAttendeesForRequestedEvent = true;
+
+    for (int i = 0; i < listOfEvents.size(); i++) {
+      Set<String> eventAttendees = listOfEvents.get(i).getAttendees();
+
+      for (String nameInEvent : eventAttendees) {
+        for (String nameInRequestedEvent : requestedEventAttendees) {
+          if (nameInEvent == nameInRequestedEvent) {
+            noMatchingAttendeesForRequestedEvent = false;
+            break;
+          }
+        }
+        // Bubble answer up
+        if (!noMatchingAttendeesForRequestedEvent) break;
+      }
+      // Bubble answer up
+      if (!noMatchingAttendeesForRequestedEvent) break;
+    }
+
+    // If the meeting requested has no members from any other meetings during the day then that meeting can occur any day.
+    if (noMatchingAttendeesForRequestedEvent) return Arrays.asList(TimeRange.WHOLE_DAY);
+
+
+    // Compress Events 
+    ArrayList<TimeRange> eventsRange = compressEvents(listOfEvents);
+
+    // Check to make sure event duration can fit.
+    int currentEventMeetingDuration = 0;
+    for (int i = 0; i < eventsRange.size(); i++) {
+      currentEventMeetingDuration += eventsRange.get(i).end() -  eventsRange.get(i).start();
+    }
+  
+    if (request.getDuration() + currentEventMeetingDuration > TimeRange.WHOLE_DAY.duration()) return Arrays.asList();
+
+    // Get possible meeting slots
+    return getPossibleRanges(eventsRange);
+  }
+
+  public ArrayList<TimeRange> compressEvents(ArrayList<Event> listOfEvents) {
+    ArrayList<TimeRange> eventsRange = new ArrayList<TimeRange>();
+    // Loop events and combine overlapping events as part of same event
+    for (int i = 0; i < listOfEvents.size(); i++) {
+      // If overlap we try to extend event.
+      if (i != 0 && listOfEvents.get(i).getWhen().overlaps(listOfEvents.get(i - 1).getWhen())) {
+        int overlappingEventEndTime = listOfEvents.get(i).getWhen().end();
+
+        int previousEventStartTime = listOfEvents.get(i - 1).getWhen().start();
+        int previousEventEndTime = listOfEvents.get(i - 1).getWhen().end();
+
+        // We are altering ONLY THE PREVIOUS event! We're simply asking the question: Can I extend this event to the right more?
+        if (overlappingEventEndTime > previousEventEndTime) eventsRange.set(eventsRange.size() - 1, TimeRange.fromStartEnd(previousEventStartTime, overlappingEventEndTime, false));
+      } else {
+        // We create a new event, since this one doesn't overlap!
+        eventsRange.add(listOfEvents.get(i).getWhen());
+      }
+    }
+    return eventsRange;
+  }
+
+  public ArrayList<TimeRange> getPossibleRanges(ArrayList<TimeRange> eventsRange) {
+    ArrayList<TimeRange> possibleRanges = new ArrayList<TimeRange>();
+
+    // Try to get the first range if first meeting doesn't start at 0.
+    if (eventsRange.get(0).start() != 0) {
+      possibleRanges.add(TimeRange.fromStartEnd(0, eventsRange.get(0).start(), false));
+    }
+
+    // Loop through and get ranges.
+    //   |-----|   |-----| == Events
+    //         |---|       == Range we want to get.
+    //
+    for (int i = 1; i < eventsRange.size(); i = i + 2) {
+      int nextEventStartTime = eventsRange.get(i).start();
+      int lastEventEndTime = eventsRange.get(i - 1).end();
+      possibleRanges.add(TimeRange.fromStartEnd(lastEventEndTime, nextEventStartTime, false));
+    }
+
+    // Try to get the last range if last event doesn't end at 1440.
+    if (eventsRange.get(eventsRange.size() - 1).end() != 1440) {
+      int lastEventEndTime = eventsRange.get(eventsRange.size() - 1).end();
+      possibleRanges.add(TimeRange.fromStartEnd(lastEventEndTime, 1440, false));
+    }
+
+    return possibleRanges;
   }
 }
